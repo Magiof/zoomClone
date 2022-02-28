eba41470018a2c33302ede91b322fa4a9c8495bb
seongwon <87584033+Magiof@users.noreply.github.com>
2022-02-28 16:09:41 +0900

    ws without socketIO

--- src/server.js
+++ src/server.js
@@ -20,10 +20,30 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket) {
-    console.log(socket);
+function onSocketClose() {
+    console.log('Disconnected from the Browser ❌');
 }
 
-wss.on('connection', handleConnection);
+const sockets = [];
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    socket['nickname'] = 'Anon';
+    console.log('Connected to Browser ✅');
+    socket.on('close', onSocketClose);
+    socket.on('message', (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case 'new_message':
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+                break;
+            case 'nickname':
+                socket['nickname'] = message.payload;
+                break;
+        }
+    });
+});
 
 server.listen(3000, handleListen);

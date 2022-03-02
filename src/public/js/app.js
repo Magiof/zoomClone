66d697dcfd17743046a086d29e1afd5f466d23f9
seongwon <87584033+Magiof@users.noreply.github.com>
2022-03-02 22:16:56 +0900

    with webRTC

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,15 @@ const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
 
+const call = document.getElementById('call');
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
     try {
@@ -18,7 +24,7 @@ async function getCameras() {
             const option = document.createElement('option');
             option.value = camera.deviceId;
             option.innerText = camera.label;
-            if(currentCamera.label===camera.label){
+            if (currentCamera.label === camera.label) {
                 option.selected = true;
             }
             camerasSelect.appendChild(option);
@@ -42,15 +48,13 @@ async function getMedia(deviceId) {
             deviceId ? cameraConstraints : initialConstraints
         );
         myFace.srcObject = myStream;
-        if(!deviceId){
+        if (!deviceId) {
             await getCameras();
         }
     } catch (e) {
         console.log(e);
     }
 }
-
-getMedia();
 
 function handleMuteClick() {
     myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
@@ -75,11 +79,88 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+            .getSenders()
+            .find((sender) => sender.track.kind === 'video');
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
 camerasSelect.addEventListener('input', handleCameraChange);
+
+//Welcome Form (choose a room)
+
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
+
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector('input');
+    await initCall();
+    socket.emit('join_room', input.value);
+    roomName = input.value;
+    input.value = '';
+}
+
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+//socket code
+
+socket.on('welcome', async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit('offer', offer, roomName);
+});
+
+socket.on('offer', async (offer) => {
+    console.log('received the offer');
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    console.log(answer);
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit('answer', answer, roomName);
+    console.log('sent the answer');
+});
+
+socket.on('answer', (answer) => {
+    console.log('received the answer');
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+    console.log('received candidate');
+    myPeerConnection.addIceCandidate(ice);
+});
+
+//RTC code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener('icecandidate', handleIce);
+    myPeerConnection.addEventListener('addstream', handleAddStream);
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    console.log('sent candidate');
+    socket.emit('ice', data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peersFace = document.getElementById('peersFace');
+    peersFace.srcObject = data.stream;
+}
 //여기부터 채팅
 // const welcome = document.getElementById('welcome');
 // const form = welcome.querySelector('form');
--- src/server.js
+++ src/server.js
@@ -25,6 +25,22 @@ instrument(wsServer, {      //admin-ui를 사용하기위한 세팅
     auth: false,    //여기서 비밀번호 세팅할 수 있음
 });
 
+wsServer.on("connection", socket =>{
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    })
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    })
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice)
+    })
+})
+
 // 여기서부터 socket io를 활용한 채팅기능
 // function publicRooms() {
 //     const {
